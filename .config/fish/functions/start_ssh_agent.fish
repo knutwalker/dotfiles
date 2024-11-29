b54861400d5b0e74f258b1721c39426b6d67aa5a
Paul Horn <dev@knutwalker.engineer>
2024-11-29 20:26:16 +0100

    Use fish compatible ssh-agent script

--- .config/fish/functions/start_ssh_agent.fish
+++ .config/fish/functions/start_ssh_agent.fish
@@ -1,8 +1,10 @@
 function start_ssh_agent
     # Avoid ssh agent spawning new instances on every shell
-    set ssh_agent_out $(ssh-agent -a $SSH_AUTH_SOCK -s 2>/dev/null)
+    set ssh_agent_out $(ssh-agent -a $SSH_AUTH_SOCK -c 2>/dev/null)
     if test $status -eq 0
         eval $ssh_agent_out > /dev/null
+        set -gx SSH_AUTH_SOCK $SSH_AUTH_SOCK
+        set -gx SSH_AGENT_PID $SSH_AGENT_PID
     else
         set -gx SSH_AGENT_PID $(pgrep ssh-agent)
     end
